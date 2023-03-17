527e7c9f71f2b6329e2b56cd293ae434ad2331cb
Melkor55 <alexhanc2000@yahoo.com>
2023-03-17 20:31:11 +0200

    generated functions to get data from the database

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SE2023
 
+## `npm install` to install the required packages(the dependencies from package.json)
+
 ## `npm start` to run -> `inferenta` part
 
 ## `cd database` + `node database.js` to run `bazaCunostinte` part
---
+++ entity/Car.js
@@ -0,0 +1,17 @@
+class Car{
+    constructor(Brand,Model,Body,Year,Type,TransmissionType,Horsepower,Seats,Doors,Price)
+    {   
+        this.Brand = Brand;
+        this.Model = Model;
+        this.Body = Body;
+        this.Year = Year;
+        this.Type = Type;
+        this.TransmissionType = TransmissionType;
+        this.Horsepower = Horsepower;
+        this.Seats = Seats;
+        this.Doors = Doors;
+        this.Price  = Price;
+    }
+}
+
+module.exports = Car;
